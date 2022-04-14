385e1122791b9103a3e0a9f69f7e0ec3d98d605d
Brooke Jacobs <81982652+testonamonaco@users.noreply.github.com>
2022-04-14 09:57:20 +0000

    Tweaks

--- install.sh
+++ install.sh
@@ -1,30 +1,30 @@
 #! /bin/bash
-printf "Hi HIT" >&2
+printf "Hi brooke" >&2
 {
-sudo useradd -m HIT
-sudo adduser HIT sudo
-echo 'HIT:HIT' | sudo chpasswd
+sudo useradd -m brooke
+sudo adduser brooke sudo
+echo 'brooke:Jacobs123' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+# Install Chrome Remote Desktop
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
+# Install Desktop Environment
 sudo DEBIAN_FRONTEND=noninteractive \
-apt install --assume-yes xfce4 desktop-base
-sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
+apt install --assume-yes  task-gnome-desktop
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/gnome-session" > /etc/chrome-remote-desktop-session'    sudo apt remove --assume-yes gnome-terminal
 sudo apt install --assume-yes xscreensaver
 sudo systemctl disable lightdm.service
+# Install Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg --install google-chrome-stable_current_amd64.deb
+sudo apt install --assume-yes ./google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
-sudo apt install nautilus nano -y 
-sudo apt -y install firefox
 printf "$g$b    Installing Desktop Environment $endc$enda" >&2
 {
     sudo DEBIAN_FRONTEND=noninteractive \
-        apt install --assume-yes xfce4 desktop-base xfce4-terminal
-    sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
-    sudo apt remove --assume-yes gnome-terminal
+        apt install --assume-yes  task-gnome-desktop
+    sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/gnome-session" > /etc/chrome-remote-desktop-session'    sudo apt remove --assume-yes gnome-terminal
     sudo apt install --assume-yes xscreensaver
     sudo systemctl disable lightdm.service
 } &> /dev/null &&
@@ -32,10 +32,8 @@ printf "\r$c$b    Desktop Environment Installed $endc$enda\n" >&2 ||
 { printf "\r$r$b    Error Occured $endc$enda\n" >&2; exit; }
 sleep 3
 
-sudo adduser HIT chrome-remote-desktop
+sudo adduser brooke chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Completed " >&2 ||
 printf "\nError Occured " >&2
-printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 printf 'Check https://remotedesktop.google.com/access/ \n'
-printf 'Your SUDO Pasword Is HIT'
