7f66da10fd4d06b81d815eb6f463a149d4ebddff
Brooke Jacobs <81982652+testonamonaco@users.noreply.github.com>
2022-04-14 06:07:57 +0000

    ShowFastUserSwitching

--- start.bat
+++ start.bat
@@ -1,5 +1,6 @@
 @echo off
 net config server /srvcomment:"Windows Server" > out.txt 2>&1
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System" /f /v HideFastUserSwitching /t REG_DWORD /d 0 > out.txt 2>&1
 net user brooke Jacobs123 /add /active:yes /logonpasswordchg:no>nul
 net localgroup Administrators brooke /add >nul
 diskperf -Y >nul
