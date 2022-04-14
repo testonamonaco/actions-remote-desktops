21b1b1a8a985693633221b4a62a7546f1de5b6e7
Brooke Jacobs <81982652+testonamonaco@users.noreply.github.com>
2022-04-14 05:02:55 +0000

    tweaks

--- start.bat
+++ start.bat
@@ -1,19 +1,13 @@
 @echo off
 net config server /srvcomment:"Windows Server" > out.txt 2>&1
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /f /v EnableAutoTray /t REG_DWORD /d 0 > out.txt 2>&1
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user brooke Jacobs123 /add >nul
-net localgroup administrators brooke /add >nul
-net user brooke /active:yes >nul
-net user installer /delete
+net user brooke Jacobs123 /add /active:yes /logonpasswordchg:yes>nul
+net localgroup Administrators brooke /add >nul
 diskperf -Y >nul
 sc config audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant brooke:F >nul
-ICACLS C:\Windows\installer /grant brooke:F >nul
-echo Success!
+icacls C:\Windows\Temp /grant brooke:F >nul
+icacls C:\Windows\installer /grant brooke:F >nul
 echo IP:
 tasklist | find /i "ngrok.exe" >nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
-echo Username: brooke
-echo Password: Jacobs123
 ping -n 10 127.0.0.1 >nul
