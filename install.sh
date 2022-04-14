85b3ea7499dbaa925b6ffd7935fa3e8149828515
Brooke Jacobs <81982652+testonamonaco@users.noreply.github.com>
2022-04-14 08:44:10 +0000

    Create file

---
+++ install.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+printf "Hi HIT" >&2
+{
+sudo useradd -m HIT
+sudo adduser HIT sudo
+echo 'HIT:HIT' | sudo chpasswd
+sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+sudo apt-get update
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo DEBIAN_FRONTEND=noninteractive \
+apt install --assume-yes xfce4 desktop-base
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
+sudo apt install --assume-yes xscreensaver
+sudo systemctl disable lightdm.service
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg --install google-chrome-stable_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+wget https://media.codeweavers.com/pub/crossover/cxlinux/demo/crossover_20.0.2-1.deb
+sudo dpkg -i crossover_20.0.2-1.deb
+sudo apt install --assume-yes --fix-broken
+sudo apt install nautilus nano -y 
+sudo apt -y install firefox
+printf "$g$b    Installing Desktop Environment $endc$enda" >&2
+{
+    sudo DEBIAN_FRONTEND=noninteractive \
+        apt install --assume-yes xfce4 desktop-base xfce4-terminal
+    sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
+    sudo apt remove --assume-yes gnome-terminal
+    sudo apt install --assume-yes xscreensaver
+    sudo systemctl disable lightdm.service
+} &> /dev/null &&
+printf "\r$c$b    Desktop Environment Installed $endc$enda\n" >&2 ||
+{ printf "\r$r$b    Error Occured $endc$enda\n" >&2; exit; }
+sleep 3
+
+sudo adduser HIT chrome-remote-desktop
+} &> /dev/null &&
+printf "\nSetup Completed " >&2 ||
+printf "\nError Occured " >&2
+printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
+printf 'Check https://remotedesktop.google.com/access/ \n'
+printf 'Your SUDO Pasword Is HIT'
