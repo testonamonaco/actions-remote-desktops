df720a36b9258f76044b8b384504e74c5d42b61e
Brooke Jacobs <81982652+testonamonaco@users.noreply.github.com>
2022-04-14 10:45:35 +0000

    Revert to XFCE4

--- install.sh
+++ install.sh
@@ -18,11 +18,11 @@ sudo apt install --assume-yes ./google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 printf "$g$b    Installing Desktop Environment $endc$enda" >&2
 {
-    sudo DEBIAN_FRONTEND=noninteractive \
-        apt install --assume-yes  task-gnome-desktop
-    sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/gnome-session" > /etc/chrome-remote-desktop-session'    sudo apt remove --assume-yes gnome-terminal
-    sudo apt install --assume-yes xscreensaver
-    sudo systemctl disable lightdm.service
+        sudo DEBIAN_FRONTEND=noninteractive \
+        apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver
+        sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
+        sudo apt install --assume-yes xscreensaver
+        sudo systemctl disable lightdm.service
 } &> /dev/null &&
 printf "\r$c$b    Desktop Environment Installed $endc$enda\n" >&2 ||
 { printf "\r$r$b    Error Occured $endc$enda\n" >&2; exit; }
