2788c07a54d23f58cfe91d2768998da491617de2
Brooke Jacobs <81982652+testonamonaco@users.noreply.github.com>
2022-06-01 00:31:25 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -8,5 +8,5 @@ sc start audiosrv > nul
 icacls C:\Windows\Temp /grant brooke:F > nul
 icacls C:\Windows\installer /grant brooke:F > nul
 echo IP:
-tasklist | find /i "ngrok.exe" > nul && curl -s localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url' || echo "Failed to retreive NGROK authtoken - check again your authtoken"
+tasklist | find /i "ngrok.exe" > nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
 ping -n 10 127.0.0.1 > nul
