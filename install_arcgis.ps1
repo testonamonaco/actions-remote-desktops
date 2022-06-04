42797c114fa03be52bfcb78877cd171aad71bee5
Brooke Jacobs <81982652+testonamonaco@users.noreply.github.com>
2022-06-04 13:25:19 +0000

    Revert

--- install_arcgis.ps1
+++ install_arcgis.ps1
@@ -1,5 +1,5 @@
 Invoke-WebRequest 'https://www.googleapis.com/drive/v3/files/1eZaEe6mEvOQIdiUJbtFR6EOqqsljYu06?alt=media&key=AIzaSyBD8tY90tF5DojRDNgAHN00gfoTCNOiuAI' -OutFile ArcGIS_Setup.zip
 Expand-Archive -Path ArcGIS_Setup.zip -DestinationPath C:\ArcGIS\ -Verbose
-Msiexec /i C:\ArcGIS\setup.msi INSTALLDIR="C:\Program Files(x86)\ArcGIS" ESRI_LICENSE_HOST=GIS SOFTWARE_CLASS=Professional SEAT_PREFERENCE=Float /qb
+Msiexec /i C:\ArcGIS\setup.msi INSTALLDIR="C:\ArcGIS" ESRI_LICENSE_HOST=GIS SOFTWARE_CLASS=Professional SEAT_PREFERENCE=Float /qb
 Invoke-WebRequest 'https://raw.githubusercontent.com/testonamonaco/actions-remote-desktops/main/AfCore.dll' -OutFile AfCore.dll
 # Copy-Item AfCore.dll -Destination "C:\ArcGIS\Desktop10.8\bin"
