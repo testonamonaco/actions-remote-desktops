395020544684ce9c4382d0b7af6e74310aaa0e4b
Brooke Jacobs <81982652+testonamonaco@users.noreply.github.com>
2022-06-04 04:12:19 +0000

    Commented Copy-Item

--- install_globalmapper.ps1
+++ install_globalmapper.ps1
@@ -2,5 +2,5 @@ Invoke-WebRequest 'https://www.googleapis.com/drive/v3/files/1mTeknzgsG_tkl491L2
 Expand-Archive -Path 'Global Mapper 22.0.zip' -DestinationPath 'C:\Users\brooke\Documents\GM' -Verbose
 & 'C:\Users\brooke\Documents\GM\global_mapper-22.exe' /s
 Invoke-WebRequest 'https://raw.githubusercontent.com/testonamonaco/actions-remote-desktops/main/AfCore.dll' -OutFile AfCore.dll
-Copy-Item "C:\Users\brooke\Documents\GM\Crack\global_mapper.exe" -Destination "C:\Program Files\GlobalMapper22.0_64bit"
-Copy-Item -Path "C:\Users\brooke\Documents\GM\Crack\ProgramData" -Destination "C:\" -Recurse
+# Copy-Item "C:\Users\brooke\Documents\GM\Crack\global_mapper.exe" -Destination "C:\Program Files\GlobalMapper22.0_64bit"
+# Copy-Item -Path "C:\Users\brooke\Documents\GM\Crack\ProgramData" -Destination "C:\" -Recurse
