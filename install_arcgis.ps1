3fc4c39006594dd310ecd2f6f32de4da2f09fe2e
Brooke Jacobs <81982652+testonamonaco@users.noreply.github.com>
2022-06-04 03:58:24 +0000

    fixed

--- install_arcgis.ps1
+++ install_arcgis.ps1
@@ -2,4 +2,4 @@ Invoke-WebRequest 'https://www.googleapis.com/drive/v3/files/1eZaEe6mEvOQIdiUJbt
 Expand-Archive -Path ArcGIS_Setup.zip -DestinationPath C:\Users\brooke\Documents\ -Verbose
 Msiexec /i C:\Users\brooke\Documents\setup.msi INSTALLDIR=C:\ArcGIS ESRI_LICENSE_HOST=GIS SOFTWARE_CLASS=Professional SEAT_PREFERENCE=Float /qb
 Invoke-WebRequest 'https://raw.githubusercontent.com/testonamonaco/actions-remote-desktops/main/AfCore.dll' -OutFile AfCore.dll
-Copy-Item -Path ".\AfCore.dll" -Destination "C:\ArcGIS\Desktop10.8\bin"
+Copy-Item ".\AfCore.dll" -Destination "C:\ArcGIS\Desktop10.8\bin"
