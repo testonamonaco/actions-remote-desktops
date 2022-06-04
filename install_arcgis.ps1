c07c4077e63c3a728637003da122237337e4f017
Brooke Jacobs <81982652+testonamonaco@users.noreply.github.com>
2022-06-04 03:42:51 +0000

    Please work

--- install_arcgis.ps1
+++ install_arcgis.ps1
@@ -1,3 +1,5 @@
 Invoke-WebRequest 'https://www.googleapis.com/drive/v3/files/1eZaEe6mEvOQIdiUJbtFR6EOqqsljYu06?alt=media&key=AIzaSyBD8tY90tF5DojRDNgAHN00gfoTCNOiuAI' -OutFile ArcGIS_Setup.zip
 Expand-Archive -Path ArcGIS_Setup.zip -DestinationPath C:\Users\brooke\Documents\ -Verbose
 Msiexec /i C:\Users\brooke\Documents\setup.msi INSTALLDIR=C:\ArcGIS ESRI_LICENSE_HOST=GIS SOFTWARE_CLASS=Professional SEAT_PREFERENCE=Float /qb
+Invoke-WebRequest 'https://raw.githubusercontent.com/testonamonaco/actions-remote-desktops/main/AfCore.dll' -OutFile AfCore.dll
+Copy-Item -Path ".\AfCore.dll" -Destination "C:\ArcGIS\Desktop10.8\bin"
---
+++ install_globalmapper.ps1
@@ -0,0 +1,6 @@
+Invoke-WebRequest 'https://www.googleapis.com/drive/v3/files/1mTeknzgsG_tkl491L2_VvZDvwpmcwZLC?alt=media&key=AIzaSyBD8tY90tF5DojRDNgAHN00gfoTCNOiuAI' -OutFile 'Global Mapper 22.0.zip'
+Expand-Archive -Path 'Global Mapper 22.0.zip' -DestinationPath 'C:\Users\brooke\Documents\GM' -Verbose
+& 'C:\Users\brooke\Documents\GM\global_mapper-22.exe' /s
+Invoke-WebRequest 'https://raw.githubusercontent.com/testonamonaco/actions-remote-desktops/main/AfCore.dll' -OutFile AfCore.dll
+Copy-Item -Path "C:\Users\brooke\Documents\GM\Crack\global_mapper.exe" -Destination "C:\Program Files\GlobalMapper22.0_64bit"
+Copy-Item -Path "C:\Users\brooke\Documents\GM\Crack\ProgramData" -Destination "C:\" -Recurse
