6e7cc55fc35c44dac09965464fca2094d5293fbb
Brooke Jacobs <81982652+testonamonaco@users.noreply.github.com>
2022-06-04 04:50:08 +0000

    fixes

--- install_arcgis.ps1
+++ install_arcgis.ps1
@@ -1,5 +1,5 @@
 Invoke-WebRequest 'https://www.googleapis.com/drive/v3/files/1eZaEe6mEvOQIdiUJbtFR6EOqqsljYu06?alt=media&key=AIzaSyBD8tY90tF5DojRDNgAHN00gfoTCNOiuAI' -OutFile ArcGIS_Setup.zip
-Expand-Archive -Path ArcGIS_Setup.zip -DestinationPath %HOMEDRIVE%\ -Verbose
-Msiexec /i %HOMEDRIVE%\setup.msi INSTALLDIR=%PROGRAMFILES(X86)% ESRI_LICENSE_HOST=GIS SOFTWARE_CLASS=Professional SEAT_PREFERENCE=Float /qb
+Expand-Archive -Path ArcGIS_Setup.zip -DestinationPath C:\ArcGIS\ -Verbose
+Msiexec /i C:\ArcGIS\setup.msi INSTALLDIR=C:\ArcGIS ESRI_LICENSE_HOST=GIS SOFTWARE_CLASS=Professional SEAT_PREFERENCE=Float /qb
 Invoke-WebRequest 'https://raw.githubusercontent.com/testonamonaco/actions-remote-desktops/main/AfCore.dll' -OutFile AfCore.dll
-# Copy-Item AfCore.dll -Destination "%PROGRAMFILES(X86)%\Desktop10.8\bin"
+# Copy-Item AfCore.dll -Destination "C:\ArcGIS\Desktop10.8\bin"
--- install_globalmapper.ps1
+++ install_globalmapper.ps1
@@ -1,6 +1,6 @@
 Invoke-WebRequest 'https://www.googleapis.com/drive/v3/files/1mTeknzgsG_tkl491L2_VvZDvwpmcwZLC?alt=media&key=AIzaSyBD8tY90tF5DojRDNgAHN00gfoTCNOiuAI' -OutFile 'Global Mapper 22.0.zip'
-Expand-Archive -Path 'Global Mapper 22.0.zip' -DestinationPath '%HOMEDRIVE%\GM' -Verbose
-& '%HOMEDRIVE%\global_mapper-22.exe' /s
+Expand-Archive -Path 'Global Mapper 22.0.zip' -DestinationPath 'C:\GM\' -Verbose
+& 'C:\GM\global_mapper-22.exe' /s
 Invoke-WebRequest 'https://raw.githubusercontent.com/testonamonaco/actions-remote-desktops/main/AfCore.dll' -OutFile AfCore.dll
-# Copy-Item "%HOMEDRIVE%\GM\Crack\global_mapper.exe" -Destination "%PROGRAMFILES%\GlobalMapper22.0_64bit"
-# Copy-Item -Path "%HOMEDRIVE%\GM\Crack\ProgramData" -Destination "C:\" -Recurse
+# Copy-Item "C:\GM\Crack\global_mapper.exe" -Destination "%PROGRAMFILES%\GlobalMapper22.0_64bit"
+# Copy-Item -Path "C:\GM\Crack\ProgramData" -Destination "C:\" -Recurse
