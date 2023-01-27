6184961dd7db9092233bc8e518d2a2039570b6ce
nowistime <110319135+nowistime@users.noreply.github.com>
2023-01-28 00:59:10 +0100

    Tweaks

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
-net config server /srvcomment:"Windows Server" > out.txt 2 >&1
+:: net config server /hidden:yes /srvcomment:"Windows Server" > out.txt 2 >&1
 net user brooke Jacobs123 /add /active:yes /logonpasswordchg:no > nul
 :: $Password = Read-Host -AsSecureString
 :: New-LocalUser "brooke" -Password $Password
@@ -11,7 +11,9 @@ powershell -command " Set-Service -Name 'audiosrv' -StartupType Automatic "
 :: sc start audiosrv > nul
 powershell -command " Start-Service -Name 'audiosrv' "
 icacls C:\Windows\Temp /grant brooke:F > nul
+:: Set-Acl -Path "C:\Windows\Temp"
 icacls C:\Windows\installer /grant brooke:F > nul
+:: Set-Acl -Path "C:\Windows\installer"
 echo IP:
 tasklist | find /i "ngrok.exe" > nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
 ping -n 10 127.0.0.1 > nul
