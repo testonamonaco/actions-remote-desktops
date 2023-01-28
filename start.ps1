65dd476691ecaaeb4f8e65377e76eb368f2a2d66
nowistime <110319135+nowistime@users.noreply.github.com>
2023-01-28 23:44:56 +0100

    Use PowerShell script file instead of Batch file

---
+++ start.ps1
@@ -0,0 +1,37 @@
+# net config server /hidden:yes /srvcomment:"Windows Server" > out.txt 2 >&1
+net user brooke Jacobs123 /add /active:yes /logonpasswordchg:no > nul
+# $Password = ConvertTo-SecureString 'Jacobs123' -AsPlainText -Force
+# New-LocalUser 'brooke' -Password $Password
+# net localgroup Administrators brooke /add > nul
+Add-LocalGroupMember -Group 'Administrators' -Member 'brooke'
+
+diskperf -y > nul
+
+# sc config audiosrv start= auto > nul
+Set-Service -Name 'audiosrv' -StartupType Automatic
+
+# sc start audiosrv > nul
+Start-Service -Name 'audiosrv'
+
+# icacls C:\Windows\Temp /grant brooke:F > nul
+# Add-Acl -Path 'C:\Windows\Temp' -Account 'brooke' -AccessRights 'FullControl'
+# icacls C:\Windows\installer /grant brooke:F > nul
+# Add-Acl -Path 'C:\Windows\installer' -Account 'brooke' -AccessRights 'FullControl'
+
+# echo IP:
+Write-Output 'IP:'
+
+tasklist | find /i "ngrok.exe" > nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
+# $response = Invoke-WebRequest -Uri 'http://localhost:4040/api/tunnels' -Method GET -UseBasicParsing
+# $responseJson = $response.Content | ConvertFrom-Json
+# $responseJson.tunnels[0].public_url
+
+# if (Get-Process ngrok -ErrorAction SilentlyContinue) {
+#     $response = Invoke-WebRequest -Uri "http://localhost:4040/api/tunnels" -Method GET -UseBasicParsing
+#     $responseJson = $response.Content | ConvertFrom-Json
+#     $responseJson.tunnels[0].public_url
+# } else {
+#     "Failed to retreive NGROK authtoken - check again your authtoken"
+# }
+
+# Test-Connection -Count 10 -ComputerName '127.0.0.1'
