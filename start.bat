b16c4e804e32b4b574a90724c89be6cf8377f7e7
nowistime <110319135+nowistime@users.noreply.github.com>
2023-01-28 22:00:40 +0100

    Update

--- start.bat
+++ start.bat
@@ -1,21 +1,32 @@
 @echo off
+
 :: net config server /hidden:yes /srvcomment:"Windows Server" > out.txt 2 >&1
 net user brooke Jacobs123 /add /active:yes /logonpasswordchg:no > nul
 :: powershell -command " $Password = ConvertTo-SecureString 'Jacobs123' -AsPlainText -Force "
 :: powershell -command " New-LocalUser 'brooke' -Password $Password "
 :: net localgroup Administrators brooke /add > nul
 powershell -command " Add-LocalGroupMember -Group 'Administrators' -Member 'brooke' "
+
 diskperf -y > nul
+
 :: sc config audiosrv start= auto > nul
 powershell -command " Set-Service -Name 'audiosrv' -StartupType Automatic "
+
 :: sc start audiosrv > nul
 powershell -command " Start-Service -Name 'audiosrv' "
+
 :: icacls C:\Windows\Temp /grant brooke:F > nul
-powershell -command " Add-Acl -Path 'C:\Windows\Temp' -Account 'brooke' -AccessRights 'FullControl' "
+:: powershell -command " Add-Acl -Path 'C:\Windows\Temp' -Account 'brooke' -AccessRights 'FullControl' "
 :: icacls C:\Windows\installer /grant brooke:F > nul
-powershell -command " Add-Acl -Path 'C:\Windows\installer' -Account 'brooke' -AccessRights 'FullControl' "
+:: powershell -command " Add-Acl -Path 'C:\Windows\installer' -Account 'brooke' -AccessRights 'FullControl' "
+
+:: echo IP:
 powershell -command " Write-Output 'IP:' "
-tasklist | find /i "ngrok.exe" > nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
+
+:: tasklist | find /i "ngrok.exe" > nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
+powershell -command " $response = Invoke-WebRequest -Uri 'http://localhost:4040/api/tunnels' -Method GET -UseBasicParsing "
+powershell -command " $responseJson = $response.Content | ConvertFrom-Json "
+powershell -command " $responseJson.tunnels[0].public_url "
 
 :: if (Get-Process ngrok -ErrorAction SilentlyContinue) {
 ::     $response = Invoke-WebRequest -Uri "http://localhost:4040/api/tunnels" -Method GET -UseBasicParsing
@@ -25,4 +36,4 @@ tasklist | find /i "ngrok.exe" > nul && curl -s localhost:4040/api/tunnels | jq 
 ::     "Failed to retreive NGROK authtoken - check again your authtoken"
 :: }
 
-powershell -command " Test-Connection -Count 10 -ComputerName '127.0.0.1' "
+:: powershell -command " Test-Connection -Count 10 -ComputerName '127.0.0.1' "
