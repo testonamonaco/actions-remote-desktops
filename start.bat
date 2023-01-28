9e25be9ff8b6bf37846580fcfd9442f958f1ce49
nowistime <110319135+nowistime@users.noreply.github.com>
2023-01-28 21:51:08 +0100

    Tweaks

--- start.bat
+++ start.bat
@@ -11,9 +11,18 @@ powershell -command " Set-Service -Name 'audiosrv' -StartupType Automatic "
 :: sc start audiosrv > nul
 powershell -command " Start-Service -Name 'audiosrv' "
 :: icacls C:\Windows\Temp /grant brooke:F > nul
-powershell Add-Acl -Path "C:\Windows\Temp" -Account "brooke" -AccessRights "FullControl"
+powershell -command " Add-Acl -Path 'C:\Windows\Temp' -Account 'brooke' -AccessRights 'FullControl' "
 :: icacls C:\Windows\installer /grant brooke:F > nul
-powershell Add-Acl -Path "C:\Windows\installer" -Account "brooke" -AccessRights "FullControl"
-echo IP:
+powershell -command " Add-Acl -Path 'C:\Windows\installer' -Account 'brooke' -AccessRights 'FullControl' "
+powershell -command " Write-Output 'IP:' "
 tasklist | find /i "ngrok.exe" > nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
-ping -n 10 127.0.0.1 > nul
+
+:: if (Get-Process ngrok -ErrorAction SilentlyContinue) {
+::     $response = Invoke-WebRequest -Uri "http://localhost:4040/api/tunnels" -Method GET -UseBasicParsing
+::     $responseJson = $response.Content | ConvertFrom-Json
+::     $responseJson.tunnels[0].public_url
+:: } else {
+::     "Failed to retreive NGROK authtoken - check again your authtoken"
+:: }
+
+powershell -command " Test-Connection -Count 10 -ComputerName '127.0.0.1' "
