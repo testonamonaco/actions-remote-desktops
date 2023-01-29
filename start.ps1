aa5d6e319dd9190804092280fb555b6624337481
nowistime <110319135+nowistime@users.noreply.github.com>
2023-01-29 01:39:23 +0100

    Tweaks

--- start.ps1
+++ start.ps1
@@ -1,33 +1,37 @@
 # net config server /hidden:yes /srvcomment:"Windows Server" > out.txt 2 >&1
 # net user brooke Jacobs123 /add /active:yes /logonpasswordchg:no > nul
-$Password = ConvertTo-SecureString 'Jacobs123' -AsPlainText -Force
+$Password = ConvertTo-SecureString "Jacobs123" -AsPlainText -Force
 New-LocalUser -Name "brooke" -Password $Password -PasswordNeverExpires:$true -UserMayNotChangePassword:$true -AccountNeverExpires:$true
 # Enable-LocalUser -Name "brooke"
 
 # net localgroup Administrators brooke /add > nul
-Add-LocalGroupMember -Group 'Administrators' -Member 'brooke'
+Add-LocalGroupMember -Group "Administrators" -Member "brooke"
 
 # diskperf -y > nul
 # Enable-DiskPerf -DriveLetter * -Counters * -Force
 
 # sc config audiosrv start= auto > nul
-Set-Service -Name 'audiosrv' -StartupType Automatic
+Set-Service -Name "audiosrv" -StartupType Automatic
 
 # sc start audiosrv > nul
-Start-Service -Name 'audiosrv'
+Start-Service -Name "audiosrv"
 
 # icacls C:\Windows\Temp /grant brooke:F > nul
-# Add-Acl -Path 'C:\Windows\Temp' -Account 'brooke' -AccessRights 'FullControl'
+# Add-Acl -Path "C:\Windows\Temp" -Account "brooke" -AccessRights "FullControl"
 # icacls C:\Windows\installer /grant brooke:F > nul
-# Add-Acl -Path 'C:\Windows\installer' -Account 'brooke' -AccessRights 'FullControl'
+# Add-Acl -Path "C:\Windows\installer" -Account "brooke" -AccessRights "FullControl"
 
 # echo IP:
-Write-Output 'IP:'
+Write-Output -NoNewLine "IP: " -ForegroundColor Green
 
 # Start-Process cmd -ArgumentList " tasklist | find /i 'ngrok.exe' > nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo 'Failed to retreive NGROK authtoken - check again your authtoken' "
-$response = Invoke-WebRequest -Uri 'http://localhost:4040/api/tunnels' -Method GET -UseBasicParsing
+$response = Invoke-WebRequest -Uri "http://localhost:4040/api/tunnels" -Method GET -UseBasicParsing
 $responseJson = $response.Content | ConvertFrom-Json
-$responseJson.tunnels[0].public_url
+$url = $responseJson.tunnels[0].public_url
+
+$uri = New-Object System.Uri($url)
+$hostname = $uri.Host
+Write-Host $hostname
 
 # if (Get-Process ngrok -ErrorAction SilentlyContinue) {
 #     $response = Invoke-WebRequest -Uri "http://localhost:4040/api/tunnels" -Method GET -UseBasicParsing
@@ -37,4 +41,4 @@ $responseJson.tunnels[0].public_url
 #     "Failed to retreive NGROK authtoken - check again your authtoken"
 # }
 
-# Test-Connection -Count 10 -ComputerName '127.0.0.1'
+# Test-Connection -Count 10 -ComputerName "127.0.0.1"
