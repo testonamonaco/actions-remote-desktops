19fa4f7d478d5037655835477dfa80cf3f5243c4
hanaerachid <110319135+hanaerachid@users.noreply.github.com>
2023-08-06 15:33:52 +0100

    Twaks

--- install_arcgis.ps1
+++ install_arcgis.ps1
@@ -1,6 +1,6 @@
 # Download the archive to a folder
-Invoke-WebRequest 'https://www.googleapis.com/drive/v3/files/1pBwf4I3dehDQtpjQZYQwvKfw5CE3aynq?alt=media&key=AIzaSyBD8tY90tF5DojRDNgAHN00gfoTCNOiuAI' -OutFile "$Env:TEMP\ArcGIS_Setup.zip"
+Invoke-WebRequest 'https://www.googleapis.com/drive/v3/files/1pBwf4I3dehDQtpjQZYQwvKfw5CE3aynq?alt=media&key=AIzaSyBD8tY90tF5DojRDNgAHN00gfoTCNOiuAI' -OutFile "${Env:TEMP}\ArcGIS_Setup.zip"
 # Extract the downloaded archive to a folder
-Expand-Archive -Path "$Env:TEMP\ArcGIS_Setup.zip" -DestinationPath "C:\ArcGIS\"
+Expand-Archive -Path "${Env:TEMP}\ArcGIS_Setup.zip" -DestinationPath "C:\ArcGIS\"
 Start-Process -FilePath "C:\ArcGIS\Extract\Setup.exe" -ArgumentList "ESRI_LICENSE_HOST=GIS ACCEPTEULA=yes SOFTWARE_CLASS=Professional SEAT_PREFERENCE=Float DESKTOP_CONFIG=TRUE INSTALLDIR1=D:\python27 /qb"
-# Copy-Item "C:\ArcGIS\AfCore.dll" -Destination "C:\ArcGIS\Desktop10.8\bin"
+Copy-Item "C:\ArcGIS\Originals\AfCore.dll" -Destination "${Env:programfiles(x86)}\Desktop10.8\bin\"
