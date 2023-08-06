ddc38fe071c4c793c576e208ef7d0733bae34a76
hanaerachid <110319135+hanaerachid@users.noreply.github.com>
2023-08-06 12:44:18 +0100

    Fix

--- install_arcgis.ps1
+++ install_arcgis.ps1
@@ -2,6 +2,6 @@
 Invoke-WebRequest 'https://www.googleapis.com/drive/v3/files/1pBwf4I3dehDQtpjQZYQwvKfw5CE3aynq?alt=media&key=AIzaSyBD8tY90tF5DojRDNgAHN00gfoTCNOiuAI' -OutFile "$Env:TEMP\ArcGIS_Setup.zip"
 # Extract the downloaded archive to a folder
 Expand-Archive -Path "$Env:TEMP\ArcGIS_Setup.zip" -DestinationPath C:\ArcGIS\
-Start-Process C:\ArcGIS\setup.exe
+Start-Process -FilePath "C:\ArcGIS\setup.exe" -Wait
 Invoke-WebRequest 'https://raw.githubusercontent.com/testonamonaco/actions-remote-desktops/main/AfCore.dll' -OutFile AfCore.dll
 # Copy-Item AfCore.dll -Destination "C:\ArcGIS\Desktop10.8\bin"
