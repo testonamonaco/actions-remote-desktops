cbdeba8a31bc6aa8fdbd43a2fdd914e53dc2a17e
hanaerachid <110319135+hanaerachid@users.noreply.github.com>
2023-08-06 12:30:25 +0100

    Tweaks

--- install_arcgis.ps1
+++ install_arcgis.ps1
@@ -1,5 +1,7 @@
-Invoke-WebRequest 'https://www.googleapis.com/drive/v3/files/1pBwf4I3dehDQtpjQZYQwvKfw5CE3aynq?alt=media&key=AIzaSyBD8tY90tF5DojRDNgAHN00gfoTCNOiuAI' -OutFile ArcGIS_Setup.zip
-Expand-Archive -Path ArcGIS_Setup.zip -DestinationPath C:\ArcGIS\ -Verbose
-Msiexec /i C:\ArcGIS\setup.msi INSTALLDIR="C:\ArcGIS" ESRI_LICENSE_HOST=GIS SOFTWARE_CLASS=Professional SEAT_PREFERENCE=Float /qb
+# Download the archive to a folder
+Invoke-WebRequest 'https://www.googleapis.com/drive/v3/files/1pBwf4I3dehDQtpjQZYQwvKfw5CE3aynq?alt=media&key=AIzaSyBD8tY90tF5DojRDNgAHN00gfoTCNOiuAI' -OutFile "$Env:TEMP\ArcGIS_Setup.zip"
+# Extract the downloaded archive to a folder
+Expand-Archive -Path "$Env:TEMP\ArcGIS_Setup.zip" -DestinationPath C:\ArcGIS\
+Start-Proceess C:\ArcGIS\setup.exe
 Invoke-WebRequest 'https://raw.githubusercontent.com/testonamonaco/actions-remote-desktops/main/AfCore.dll' -OutFile AfCore.dll
 # Copy-Item AfCore.dll -Destination "C:\ArcGIS\Desktop10.8\bin"
