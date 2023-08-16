c6d07cfa637094e42c23a965f7c1af2af134049a
hanaerachid <110319135+hanaerachid@users.noreply.github.com>
2023-08-16 23:01:48 +0100

    Mount-DickImage

--- config.ps1
+++ config.ps1
@@ -19,3 +19,4 @@ Invoke-WebRequest 'https://www.googleapis.com/drive/v3/files/1-gNqJntO86HkjEXIDb
 
 # Download Adobe Illustrator
 Invoke-Expression -Command "mega-get 'https://mega.nz/file/8kcwDapC#WWLJ2fBOOmWLQQClA9mopuKR8TjNqw0PsMsOCmqr1L8' $Env:USERPROFILE/Downloads/"
+Mount-DiskImage -ImagePath "$Env:USERPROFILE/Downloads/Adobe Illustrator 2023 v27.1.1.196 (x64) Multilingual.iso"
