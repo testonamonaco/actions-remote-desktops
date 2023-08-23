ad5906ea4ae5d00a2a74c5dc5991296798a88e3e
hanaerachid <110319135+hanaerachid@users.noreply.github.com>
2023-08-23 12:42:38 +0100

    Illustrator is now installed on demand

--- config.ps1
+++ config.ps1
@@ -16,7 +16,3 @@ Invoke-WebRequest 'https://www.googleapis.com/drive/v3/files/1-gNqJntO86HkjEXIDb
 # Msiexec /i C:\ArcGIS\setup.msi INSTALLDIR="C:\ArcGIS" ESRI_LICENSE_HOST=GIS SOFTWARE_CLASS=Professional SEAT_PREFERENCE=Float /qb
 # Invoke-WebRequest 'https://raw.githubusercontent.com/testonamonaco/actions-remote-desktops/main/AfCore.dll' -OutFile AfCore.dll
 # Copy-Item AfCore.dll -Destination "C:\ArcGIS\Desktop10.8\bin"
-
-# Download Adobe Illustrator
-Invoke-Expression -Command "mega-get 'https://mega.nz/file/8kcwDapC#WWLJ2fBOOmWLQQClA9mopuKR8TjNqw0PsMsOCmqr1L8' $Env:USERPROFILE/Downloads/"
-Mount-DiskImage -ImagePath "$Env:USERPROFILE/Downloads/Adobe Illustrator 2023 v27.1.1.196 (x64) Multilingual.iso"
---
+++ install_illustrator.ps1
@@ -0,0 +1,3 @@
+# Download Adobe Illustrator
+Invoke-Expression -Command "mega-get 'https://mega.nz/file/8kcwDapC#WWLJ2fBOOmWLQQClA9mopuKR8TjNqw0PsMsOCmqr1L8' $Env:USERPROFILE/Downloads/"
+Mount-DiskImage -ImagePath "$Env:USERPROFILE/Downloads/Adobe Illustrator 2023 v27.1.1.196 (x64) Multilingual.iso"
